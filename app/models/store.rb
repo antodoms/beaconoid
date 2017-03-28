d7caccc0ed59c156ee188c9a22594057265dc4d3
antodoms <antodoms@gmail.com>
2017-03-28 23:10:11 +1100

    document type for dynamoid custom added

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,11 +1,17 @@
 class Store
 	
 	if Rails.env.development?
+
 	    include Mongoid::Document
+	    field :name, type: String
+  		field :code, type: String
+
 	elsif Rails.env.production?
+
 		include Dynamoid::Document
+	  	field :name
+  		field :code
+
 	end
-  
-  	field :name, type: String
-  	field :code, type: String
+ 
 end
