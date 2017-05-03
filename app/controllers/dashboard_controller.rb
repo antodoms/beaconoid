5e1ad3f59e8649be73751a5167092e23cb710ece
antodoms <antodoms@gmail.com>
2017-05-04 02:14:23 +1000

    added the first bar char report for number of fetch to click per each stores

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,6 @@ class DashboardController < ApplicationController
 
 
   def index
-
+  	@data = CustomerTracking.get_json(Store.all)
   end
 end
--- app/models/customer_tracking.rb
+++ app/models/customer_tracking.rb
@@ -10,6 +10,12 @@ class CustomerTracking
 	  	field :action, type: String
 	  	field :time, type: String
 
+		scope :fetch, ->{ where(action: "fetch") }
+		scope :click, ->{ where(action: "click") }
+
+		scope :fetch_with_store, ->(store){ where(action: "fetch", store_id: store) }
+		scope :click_with_store, ->(store){ where(action: "click", store_id: store) }
+
 	elsif Rails.env.production?
 		include Dynamoid::Document
 		field :customer_id
@@ -19,5 +25,24 @@ class CustomerTracking
 	  	field :beacon_id
 	  	field :action
 	  	field :time
+
+		scope :fetch, ->{ where(action: "fetch") }
+		scope :click, ->{ where(action: "click") }
+
+		scope :fetch_with_store, ->(store){ where(action: "fetch", store_id: store) }
+		scope :click_with_store, ->(store){ where(action: "click", store_id: store) }	  	
 	end
+
+	def self.get_json(stores)
+
+		final_data = []
+		final_data << ['User Interaction', 'Fetch', 'Click']
+
+		stores.each do |store|
+			final_data << [store.name, fetch_with_store(store.id).count, click_with_store(store.id).count]
+		end
+		final_data
+	end
+
+
 end
