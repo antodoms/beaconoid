711f094e25870a13fa28be05d04ea6b966cf64f5
antodoms <antodoms@gmail.com>
2017-05-13 18:14:42 +1000

    corrected the assets pipeline image binding using the rails way

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,9 +41,5 @@ set :bundle_binstubs, nil
 set :linked_files, fetch(:linked_files, []).push('config/database.yml','config/application.yml')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
-after 'deploy:publishing', 'deploy:restart'
-namespace :deploy do
-  task :restart do
-    invoke 'unicorn:reload'
-  end
-end
+after 'deploy:assets:precompile', 'unicorn:stop'
+after 'deploy:log_revision', 'unicorn:start'
