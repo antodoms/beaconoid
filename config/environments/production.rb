263936bf9f489c626382445d77a45f24db1c2b35
antodoms <antodoms@gmail.com>
2017-05-17 20:57:27 +1000

    added actioncable support for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.action_cable.url = "ws://www.beaconoid.me/cable"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
