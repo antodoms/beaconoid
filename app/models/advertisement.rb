a6ff702e6a165b006a342f0593e5cffef52e9975
antodoms <antodoms@gmail.com>
2017-05-22 22:13:31 +1000

    added the correct advertisement image to the advertisement page, corrected the upload bug and corrected the turbolink javascript load issue '

--- app/models/advertisement.rb
+++ app/models/advertisement.rb
@@ -3,22 +3,9 @@ class Advertisement < ActiveRecord::Base
   belongs_to :beacon
   belongs_to :category
 
-  has_attached_file :image, default_url: "//s3-ap-southeast-2.amazonaws.com/beaconoid/default.jpg", :path => "#{Rails.env}/image/:id/:filename"
+  has_attached_file :image, styles: { medium: "500x300", thumb: "150x100>" }, default_url: "//s3-ap-southeast-2.amazonaws.com/beaconoid/default.jpg", :path => "#{Rails.env}/image/:id/:style/:filename"
   validates :image, attachment_presence: false
-  # has_attached_file :image,
-  #                   :storage => :s3,
-  #                   :s3_region => ENV["AWS_REGION"],
-  #                   :s3_host_name => ENV["S3_HOSTNAME"],
-  #                   :path => "#{Rails.env}/image/:id/:filename",
-  #                   default_url: "//s3-ap-southeast-2.amazonaws.com/beaconoid/default.jpg",
-  #                   :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
 
 
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
-
-
-  # def s3_credentials
-  #   {:bucket => "beaconoid", :access_key_id => ENV["ACCESS_KEY_ID"], :secret_access_key => ENV["SECRET_ACCESS_KEY"]}
-  # end
- #Read more at https://www.pluralsight.com/guides/ruby-ruby-on-rails/handling-file-upload-using-ruby-on-rails-5-api#LlGBKswCkMp7h73l.99
 end
