9534852082ff07142033655f5414f91cf3f25cd7
antodoms <antodoms@gmail.com>
2017-05-22 20:11:46 +1000

    patch fix

--- app/models/advertisement.rb
+++ app/models/advertisement.rb
@@ -5,16 +5,16 @@ class Advertisement < ActiveRecord::Base
 
   #has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "//s3-ap-southeast-2.amazonaws.com/beaconoid/default.jpg"
 
-  # has_attached_file :image,
-  #                   :storage => :s3,
-  #                   :s3_region => ENV["AWS_REGION"],
-  #                   :s3_host_name => ENV["S3_HOSTNAME"],
-  #                   :path => "#{Rails.env}/image/:id/:filename",
-  #                   default_url: "//s3-ap-southeast-2.amazonaws.com/beaconoid/default.jpg",
-  #                   :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
+  has_attached_file :image,
+                    :storage => :s3,
+                    :s3_region => ENV["AWS_REGION"],
+                    :s3_host_name => ENV["S3_HOSTNAME"],
+                    :path => "#{Rails.env}/image/:id/:filename",
+                    default_url: "//s3-ap-southeast-2.amazonaws.com/beaconoid/default.jpg",
+                    :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
 
 
-  # validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
 
   def s3_credentials
