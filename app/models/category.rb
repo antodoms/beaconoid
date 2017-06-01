a9c8a0f44d9d38b7dc4e8909c2989450ee2f8e35
antodoms <antodoms@gmail.com>
2017-06-01 21:16:55 +1000

    Adding search functionality to category page. making style changes.

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,15 +4,9 @@ class Category < ApplicationRecord
 	validates_uniqueness_of :name
 	has_many :advertisements
 
+	default_scope { order(created_at: :desc) }
+
 	WillPaginate.per_page = 10
-
-	def search_filter
-	  category.try(:name)
-	end
-
-	def search_filter=(name)
-	  Category.find_by(name: name) if name.present?
-	end
 
 	def self.filter_by_name(text)
 		where("(LOWER(name) like LOWER(?)) OR (LOWER(description) like LOWER(?))", "%#{text}%", "%#{text}%")
